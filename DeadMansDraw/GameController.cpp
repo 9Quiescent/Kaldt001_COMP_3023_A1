f924a37efc460f19ddd6df5a0a3fa3c223e040c9
9Quiescent <dkalongonda@gmail.com>
2025-04-21 17:13:16 +0930

    Revert "Implement random name selection."
    This reverts commit 3b0494bd6afc7f6f65c001a5556c7d46989bcce2.

--- DeadMansDraw/GameController.cpp
+++ DeadMansDraw/GameController.cpp
@@ -10,8 +10,6 @@
 #include "MermaidCard.h"
 #include "OracleCard.h"
 #include <iostream>
-#include <cstdlib> 
-#include <ctime>
 
 GameController::GameController()
     : game(nullptr), playerController(nullptr)
@@ -29,22 +27,8 @@ void GameController::startGame()
     game = new Game();
     playerController = new HumanPlayerController();
 
-    srand(time(0)); 
-
-    std::string possibleNames[] = {
-        "Billy", "Sasha", "Mira", "Leo", "Nina", "Dante", "Aria", "Kai"
-    };
-    const int nameCount = sizeof(possibleNames) / sizeof(possibleNames[0]);
-
-    std::string name1 = possibleNames[rand() % nameCount];
-    std::string name2 = possibleNames[rand() % nameCount];
-
-    while (name1 == name2) {
-        name2 = possibleNames[rand() % nameCount];
-    }
-
-    Player* player1 = new Player(name1);
-    Player* player2 = new Player(name2);
+    Player* player1 = new Player("Billy");
+    Player* player2 = new Player("Sasha");
 
     player1->setOpponent(player2);
     player2->setOpponent(player1);
